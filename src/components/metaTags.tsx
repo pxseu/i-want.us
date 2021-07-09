6eaea4e93c84ea48a511c845d091711585dbfa70
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-09 19:05:10 +0200

    feat: break out meta tags into a comp

---
+++ src/components/metaTags.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import React, { FC } from "react";
+
+interface IMetaTags {
+	hostname: string;
+	imagePath: string;
+}
+
+const MetaTags: FC<IMetaTags> = ({ hostname, imagePath }) => (
+	<Head>
+		<meta name="msapplication-TileColor" content="#f52e77" />
+		<meta name="theme-color" content="#f52e77" />
+		<meta property="og:url" content={hostname} />
+		<meta property="og:type" content="website" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:site_name" content="i-want.us" />
+		<meta property="og:title" content="i-want.us" />
+		<meta property="og:image" content={imagePath} />
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:title" content="i-want.us" />
+		<meta name="twitter:description" content="Wanna cuddle together?" />
+		<meta name="twitter:image" content={imagePath} />
+	</Head>
+);
+
+export default MetaTags;
