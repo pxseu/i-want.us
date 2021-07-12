8954c6e00099e7bc5455ae2baa2fdbfc4d718cb4
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-13 00:57:16 +0200

    feat: waifu cards and minor css changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/comp/layout";
 import React, { FC } from "react";
 import styles from "@/styles/Homepage.module.scss";
+import WaifuCard from "@/comp/waifuCard";
 
 const Home: FC = () => (
 	<Layout>
@@ -8,6 +9,10 @@ const Home: FC = () => (
 			<h1 className={styles.slogan}>
 				Show your love <span className={styles.attention}>without</span> the stress
 			</h1>
+		</div>
+		<div className={styles.cardWrapper}>
+			<WaifuCard title="cuddle" image="/assets/cuddle-girl.png" className={styles.firstCard} />
+			<WaifuCard title="kiss" image="/assets/kiss-girl.png" className={styles.secondCard} />
 		</div>
 	</Layout>
 );
