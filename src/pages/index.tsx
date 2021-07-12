87ea72434294eb02b38c659a35ea16518282b9d2
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-12 18:45:24 +0200

    feat: base landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Layout from "@/comp/layout";
 import React, { FC } from "react";
+import styles from "@/styles/Homepage.module.scss";
 
 const Home: FC = () => (
-	<div>
-		<p>Hello</p>
-	</div>
+	<Layout>
+		<div className={styles.contentWrapper}>
+			<h1 className={styles.slogan}>
+				Show your love <span className={styles.attention}>without</span> the stress
+			</h1>
+		</div>
+	</Layout>
 );
 
 export default Home;
