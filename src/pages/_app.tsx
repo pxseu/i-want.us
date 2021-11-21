20595137301f7ce7f262a34bb0f15e7a1310beb2
pxseu <57842793+pxseu@users.noreply.github.com>
2021-11-21 04:17:52 +0100

    feat(website): icons and manifest.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -78,6 +78,15 @@ const App = ({ Component, pageProps }: AppProps) => (
 				rel="stylesheet"
 				href="https://fonts.googleapis.com/css2?family=Arvo:ital@0;1&family=Poppins:wght@600;900&display=swap"
 			/>
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/manifest.json" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color={theme.colors.brand} />
+			<meta name="apple-mobile-web-app-title" content="i-want.us" />
+			<meta name="application-name" content="i-want.us" />
+			<meta name="msapplication-TileColor" content={theme.colors.brand} />
+			<meta name="theme-color" content={theme.colors.brand} />
 		</Head>
 		<MetaTags
 			url={EmbedImg.src}
